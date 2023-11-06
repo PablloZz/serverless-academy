d180b3fcfc9ddd211711a2c598cf424291d56ef2
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-11-06 13:09:47 +0200

    + status codes enum

---
+++ rest_api_01_auth_api/src/libs/enums/status-code.enum.ts
@@ -0,0 +1,12 @@
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSED_ENTITY: 422,
+} as const;
+
+export { StatusCode };
